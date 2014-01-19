cf892cae86745904b44485befea6f12f10e7695a
Robert Charles Garth <contact@robbygenius.net>
2014-01-19 06:04:56 -0800

    RobbyGenius
    edit

--- StarterKit/WindowsAzureDeploymentProject/.templates/emulatorTools/ResetEmulator.cmd
+++
@@ -1,6 +0,0 @@
-"${EmulatorDir}\csrun.exe" /devfabric:shutdown /devfabric:clean
-"${EmulatorDir}\csrun.exe" /devstore:shutdown
-@ECHO OFF
-SET DEPLOYROOT=
-REG DELETE HKCU\Environment /V DEPLOYROOT /F 2>nul
-exit 0
